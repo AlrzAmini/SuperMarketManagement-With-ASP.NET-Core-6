207d4e08b75e15524b9c5dc4db28986574c9ad8c
AlirezaAmini <mranotmillion@gmail.com>
2022-05-21 23:20:36 +0430

    add template files

---
+++ SuperMarketManagement.Web/ViewComponents/AdminSideBarComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperMarketManagement.Web.ViewComponents
+{
+    public class AdminSideBarComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("AdminSideBar");
+        }
+    }
+}
