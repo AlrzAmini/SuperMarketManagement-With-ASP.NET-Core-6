54b9ee4800b5afa126c87561048dadaaf0907e45
AlirezaAmini <mranotmillion@gmail.com>
2022-05-22 21:12:35 +0430

    Add User Repository Add User Service

---
+++ SuperMarketManagement.Application/Services/User/UserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperMarketManagement.Application.Interfaces.User;
+
+namespace SuperMarketManagement.Application.Services.User
+{
+    public class UserService : IUserService
+    {
+    }
+}
---
+++ SuperMarketManagement.Domain/Interfaces/User/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarketManagement.Domain.Interfaces.User
+{
+    public interface IUserRepository
+    {
+    }
+}
