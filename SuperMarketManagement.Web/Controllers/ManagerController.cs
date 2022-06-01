2edb88405697ed2702fa3bcba78c662a586cd5a2
AlirezaAmini <mranotmillion@gmail.com>
2022-06-01 21:34:31 +0430

    add create manager action

--- SuperMarketManagement.Web/Controllers/ManagerController.cs
+++ SuperMarketManagement.Web/Controllers/ManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SuperMarketManagement.Application.Interfaces.User;
+using SuperMarketManagement.Domain.Models.User;
 
 namespace SuperMarketManagement.Web.Controllers
 {
@@ -13,6 +14,9 @@ namespace SuperMarketManagement.Web.Controllers
             _adminService = adminService;
         }
 
+        #region index
+
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var model = await _adminService.GetAdminsInfo();
@@ -22,7 +26,55 @@ namespace SuperMarketManagement.Web.Controllers
             }
 
             TempData[ErrorMessage] = "مدیری ثبت نشده است";
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
         }
+
+        #endregion
+
+        #region add
+
+        [HttpGet("create")]
+        public IActionResult CreateManager()
+        {
+            return View();
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateManager(Admin admin)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(admin);
+            }
+
+            if (await _adminService.AddAdmin(admin))
+            {
+                TempData[SuccessMessage] = "مدیر با موفقیت ثبت شد";
+                return RedirectToAction("Index");
+            }
+
+            TempData[ErrorMessage] = "مدیر ثبت نشد";
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        #region edit
+
+
+
+        #endregion
+
+        #region delete
+
+
+
+        #endregion
+
+        #region details
+
+
+
+        #endregion
     }
 }
